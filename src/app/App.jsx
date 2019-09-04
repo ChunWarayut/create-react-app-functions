0e480a3a8a29bc1d33fbf5c6b82b168ca2b398a3
warayut teakrathok <warayut.ta@princhealth.com>
2019-09-04 15:53:04 +0700

    Set Layout

---
+++ src/app/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import DefaultApp from '../pages/index'
+
+function App() {
+  return (<DefaultApp />);
+}
+
+export default App;
---
+++ src/pages/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../styles/App.css';
+
+function DefaultApp() {
+  return (
+    <main>
+      <h1>Create React App + Go API</h1>
+      <h2>
+        Deployed with
+        <a
+          href="https://zeit.co/docs"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          ZEIT Now
+        </a>
+        !
+      </h2>
+    </main>
+  );
+}
+
+export default DefaultApp;
